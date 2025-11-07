3c2826a185c06920d50c83e2001a19038829b06d
Luke <lukemm570@gmail.com>
2025-11-07 16:55:08 -0500

    Update copy file code

--- build.cpp
+++ build.cpp
@@ -13,7 +13,11 @@ int build(CBuild::Context context) {
     system(command.str().c_str());
     system("make -C build > /dev/null 2>&1");
 
-    fs::copy_file("build/src/libglfw3.a", "build/libglfw3.a");
+    fs::copy_file(
+        "build/src/libglfw3.a", 
+        "build/libglfw3.a", 
+        fs::copy_options::overwrite_existing
+    );
     
     return 0;
 }
